4d47597a0afc11408750bc91b1d22292faefade5
dj-dev123 <djbalila75@gmail.com>
2026-09-08 21:12:31 +0800

    Revert "Center welcome page content as a single group"
    This reverts commit 8c8249ef6361fd3d7c35153fe220d995bf98ab42.

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -6,76 +6,79 @@ export default function Welcome() {
     usePageTitle('Welcome');
 
     return (
-        <div className="min-h-screen bg-[#96b190] text-gray-800 flex items-center justify-center px-4 sm:px-6 md:px-8 py-10">
-            <div className="
-                w-full
-                max-w-md
-                sm:max-w-lg
-                md:max-w-xl
-                text-center
-            ">
-                <img
-                    src="/images/primath.png"
-                    alt="PriMath"
-                    className="
-                        mx-auto
-                        h-20
-                        sm:h-24
-                        md:h-28
-                        object-contain
-                        drop-shadow-md
-                    "
-                />
-
-                {/* Login Button */}
-                <div className="mt-10 sm:mt-12">
-                    <Link
-                        to={paths.login}
+        <div className="min-h-screen bg-[#96b190] text-gray-800 flex flex-col">
+            {/* Center Content */}
+            <main className="flex flex-1 items-center justify-center px-4 sm:px-6 md:px-8 py-10">
+                <div className="
+                    w-full
+                    max-w-md
+                    sm:max-w-lg
+                    md:max-w-xl
+                    text-center
+                ">
+                    <img
+                        src="/images/primath.png"
+                        alt="PriMath"
                         className="
-                            inline-block
-                            rounded-xl
-                            bg-green-600
-                            px-6 sm:px-8 md:px-10
-                            py-2 sm:py-3 md:py-4
-                            text-sm
-                            sm:text-base
-                            md:text-lg
-                            lg:text-xl
-                            font-semibold
-                            text-white
-                            shadow
-                            hover:bg-green-700
-                            transition-colors
+                            mx-auto
+                            h-20
+                            sm:h-24
+                            md:h-28
+                            object-contain
+                            drop-shadow-md
                         "
-                    >
-                        Log in to Start
-                    </Link>
-                </div>
+                    />
 
-                {/* Credits */}
-                <div className="mt-16 sm:mt-20">
-                    <p className="
-                        font-bold
-                        text-lg
-                        sm:text-sm
-                        md:text-base
-                        lg:text-lg
-                        text-orange-500 text-outline-white
-                    ">
-                        ROWENA B. RESPOSO
-                    </p>
-                    <p className="
-                    font-bold
-                        text-xl
-                        sm:text-sm
-                        md:text-base
-                        lg:text-lg
-                        text-black
-                    ">
-                        Master Teacher II
-                    </p>
+                    {/* Login Button */}
+                    <div className="mt-10 sm:mt-12">
+                        <Link
+                            to={paths.login}
+                            className="
+                                inline-block
+                                rounded-xl
+                                bg-green-600
+                                px-6 sm:px-8 md:px-10
+                                py-2 sm:py-3 md:py-4
+                                text-sm
+                                sm:text-base
+                                md:text-lg
+                                lg:text-xl
+                                font-semibold
+                                text-white
+                                shadow
+                                hover:bg-green-700
+                                transition-colors
+                            "
+                        >
+                            Log in to Start
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </main>
+
+            {/* Footer */}
+            <footer className="bg-[#96b190] py-3 sm:py-4 md:py-6 text-center">
+                <p className="
+                    font-bold
+                    text-lg
+                    sm:text-sm
+                    md:text-base
+                    lg:text-lg
+                    text-orange-500 text-outline-white
+                ">
+                    ROWENA B. RESPOSO
+                </p>
+                <p className="
+                font-bold
+                    text-xl
+                    sm:text-sm
+                    md:text-base
+                    lg:text-lg
+                    text-black
+                ">
+                    Master Teacher II
+                </p>
+            </footer>
         </div>
     );
 }
